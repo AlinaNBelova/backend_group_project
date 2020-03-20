a1316356cd9a93b4759f33bdd716a65ff45fb674
Alina Belova <belovaalni@gmail.com>
2020-03-20 14:38:06 -0500

    project structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(require('./routes/'));
+app.use(require('./routes/login.js'));
+app.use(require('./routes/chat.js'));
+
+app.listen(3000, () => {
+  console.log('Listening on 3000');
+});
